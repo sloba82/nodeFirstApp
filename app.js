4e76fd5a8f9d393accbf1b941f9a89959e99f8a9
s7sjuhas <sjuhas@s7designcreative.com>
2020-11-26 16:08:29 +0100

    git buitt in moduls

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const log = require('./logger');
+// built in module
+
+const path = require('path');
+const os = require('os');
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+
+var totalMemory = os.totalmem();
+console.log(totalMemory);
+
+var freeMemory = os.freemem();
+console.log(freeMemory);
+
+//templete string
+
+var temp = `total memory: ${totalMemory}
+freeMemory: ${freeMemory}`;
+
+console.log(temp);
+
+
+// file sistem
+const fs = require('fs');
+
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir( './', function (err, files) {
+    if (err) console.log('Error ', err);
+    else console.log('Files ', files);
+});
 
 
 
-log('blaaa');
 
-//console.log(test);  
+
