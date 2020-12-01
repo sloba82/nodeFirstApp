c4a774a1e8e3fad2de63007ab6d05680929d15a9
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-01 11:28:42 +0100

    listen localhost port 3000

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const EventEmitter = require('events');
+const http = require('http');
 
+const server = http.createServer();
 
-const Logger = require('./logger');
-const logger = new Logger();
-
-
-logger.on('messageLogged', (arg) => {
-    console.log('listener called', arg);
+server.on('connection', (socket) => {
+    console.log('New connection.');
 });
 
-logger.log('message');
+server.listen(3000);
+
+console.log('listening on port 3000...');
+
