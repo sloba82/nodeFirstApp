677b8e92d634a82272d8d2c422e1dbcc4e0c78cc
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-01 10:47:44 +0100

    Events module

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
-// built in module
-
-const path = require('path');
-const os = require('os');
-
-var pathObj = path.parse(__filename);
-console.log(pathObj);
-
-var totalMemory = os.totalmem();
-console.log(totalMemory);
-
-var freeMemory = os.freemem();
-console.log(freeMemory);
-
-//templete string
-
-var temp = `total memory: ${totalMemory}
-freeMemory: ${freeMemory}`;
-
-console.log(temp);
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 
-// file sistem
-const fs = require('fs');
-
-const files = fs.readdirSync('./');
-console.log(files);
-
-fs.readdir( './', function (err, files) {
-    if (err) console.log('Error ', err);
-    else console.log('Files ', files);
+emitter.on('messageLogged', (arg) => {
+    console.log('listener called', arg);
 });
 
-
-
-
-
+// sgnalizuje da se nesto desilo
+emitter.emit('messageLogged', { id:1, url: 'http://'});
