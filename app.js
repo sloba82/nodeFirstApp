608b7604df6f562a63804f3f4d0e7dbb3a85966d
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-01 11:46:19 +0100

    Http module

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
 
-server.on('connection', (socket) => {
-    console.log('New connection.');
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
 });
+
+
 
 server.listen(3000);
 
