f98f3ea82eaa1f89bce4cf18e825697d735b45fb
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-02 14:32:47 +0100

    validacija

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,7 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
-
+app.use(express.json());
 
 
 const courses = [
@@ -17,14 +18,41 @@ const courses = [
  app.get('/api/courses', (req, res) => {
      res.send(courses);
  });
+
+
+ app.post('/api/courses', (req, res) => {
+
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if(result.error){
+        //400 bad request
+       return res.status(400).send(result.error.details[0].message);
+    }
+
+     const course = {
+         id:courses.length + 1,
+         name: req.body.name
+     };
+
+     courses.push(course);
+     res.send(course);
+
+ });
+
+
+
+
  
  app.get('/api/courses/:id', (req, res) => {
      const course = courses.find(c => c.id === parseInt(req.params.id));
      if(!course) res.status(404).send('The course with the given ID was not found');
      res.send(course);
  });
-
- 
 
  app.get('/api/post/:year/:month', (req, res) => {
     res.send(req.params);
