8dc840a3b06e302067f039376414af6bc99d454c
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-03 14:24:15 +0100

    enviroment, configuration

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,6 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const config = require('config');
 const morgan = require('morgan'); // middleware, HTTP request logger middleware for node.js
 const helmet = require('helmet'); // middleware, Helmet helps you secure your Express apps by setting various HTTP headers. 
 const Joi = require('joi');
@@ -5,13 +8,8 @@ const logger = require('./logger'); // middleware
 const express = require('express');
 const app = express();
 
-
-
 //console.log(`NODE_ENV: ${process.env.NODE_ENV}`); //environment variable
 //console.log(`app: ${app.get('env')}`);
-
-
-
 
 
 app.use(express.json());
@@ -21,9 +19,17 @@ app.use(express.static('public')); // middleware  upucuje na folder public vrace
 
 app.use(helmet()); // middleware
 
+
+//configuration
+console.log( 'Application Name: ' + config.get('name'));
+console.log('Mail Serve: ' + config.get('mail.host'));
+console.log('Mail password: ' + config.get('mail.password'));
+
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    startupDebugger('Morgan enabled...');
+
+    
 }
 
 //app.use(morgan('tiny')); // Using a predefined format string
