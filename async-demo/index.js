9ade5f4dade42d17d4ae697f0625551e04326cca
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-04 14:23:19 +0100

    promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -2,8 +2,8 @@ console.log('Before');
 
 
 // Asynchronous
-getUser(1, getRepositories );
- 
+getUser(1, getRepositories);  // parent 
+
 
 console.log('After');
 
@@ -12,40 +12,38 @@ console.log('After');
 //  prvi parametar je vrednost 
 //  drugi parametar je callback funkcija, 
 // ustvari posledjuje se prvi parametar -> drugom parametru koji prima taj isti parametar kao da je funkcija
-function getRepositories(user){ 
-    getRepositories(user.gitHubUsername, getCommits);  
+function getRepositories(user) {
+    getRepositories(user.gitHubUsername, getCommits);
 }
 
-function getCommits(repos){
-    getCommits(repo, displayCommits );
-
+function getCommits(repos) {
+    getCommits(repo, displayCommits);
 }
 
-function displayCommits(commits){
-    console.log(commits); 
+function displayCommits(commits) {
+    console.log(commits);
 }
-
 
 // Callbacks
 // Promises
 // Async/await
-function getUser(id, callback)
-{
-    setTimeout(() => {
-        console.log('Reading a user form db...');
-        callback ({
-            id:id,
-            gitHubUsername: 'mosh'
-        });
-    }, 2000);
- 
-}
+// function getUser(id, callback) {
+//     setTimeout(() => {
+//         console.log('Reading a user form db...');
+//         callback({
+//             id: id,
+//             gitHubUsername: 'mosh'
+//         });
+//     }, 2000);
 
-function getRepositories(username, callback) {
-    setTimeout(() =>{
-        console.log('calling github...');
-        callback (['repo1', 'repo2', 'repo3']);
-    }, 2000);
+// }
+
+// function getRepositories(username, callback) {
+//     setTimeout(() => {
+//         console.log('calling github...');
+//         callback(['repo1', 'repo2', 'repo3']);
+//     }, 2000);
 
 
-}
+// }
+
