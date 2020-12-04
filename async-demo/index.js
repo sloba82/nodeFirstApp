c5fbb1355b728b5a31d0e055736281515de7d034
s7sjuhas <sjuhas@s7designcreative.com>
2020-12-04 15:24:39 +0100

    usage of promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,49 +1,60 @@
 console.log('Before');
 
+// getUser(1, (user) => {
+//     console.log('User', user);
+//     getRepositories(user.gitHubUsername, (repos) => {
+//         console.log('Repos ', repos);
+//     })
 
-// Asynchronous
-getUser(1, getRepositories);  // parent 
+// });
 
+
+
+getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits ' , commits))
+    .catch(err => console.log('Error ', err.message));
 
 console.log('After');
 
-//  Named functions,  
-//  getRepositories(user.gitHubUsername, getCommits);  
-//  prvi parametar je vrednost 
-//  drugi parametar je callback funkcija, 
-// ustvari posledjuje se prvi parametar -> drugom parametru koji prima taj isti parametar kao da je funkcija
-function getRepositories(user) {
-    getRepositories(user.gitHubUsername, getCommits);
-}
-
-function getCommits(repos) {
-    getCommits(repo, displayCommits);
-}
-
-function displayCommits(commits) {
-    console.log(commits);
-}
 
 // Callbacks
 // Promises
 // Async/await
-// function getUser(id, callback) {
-//     setTimeout(() => {
-//         console.log('Reading a user form db...');
-//         callback({
-//             id: id,
-//             gitHubUsername: 'mosh'
-//         });
-//     }, 2000);
+function getUser(id) {
 
-// }
-
-// function getRepositories(username, callback) {
-//     setTimeout(() => {
-//         console.log('calling github...');
-//         callback(['repo1', 'repo2', 'repo3']);
-//     }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user form db...');
+            resolve({
+                id: id,
+                gitHubUsername: 'mosh'
+            });
+        }, 2000);
+    });
 
 
-// }
+}
+
+function getRepositories(username) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('calling github...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+}
+
+
+function getCommits(repo)
+{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Callling gitHub APi...');
+            resolve(['commit']);
+        }, 2000);
+    });
+}
 
